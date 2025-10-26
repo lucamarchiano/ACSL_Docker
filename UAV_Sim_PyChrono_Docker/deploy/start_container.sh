69efe1a0c9187e6a4e8a7cc6b39f850d8ed2882e
Luca <luca.marchiano@studenti.polito.it>
2025-10-26 18:29:40 -0400

    Added VSCode support

--- UAV_Sim_PyChrono_Docker/deploy/start_container.sh
+++ UAV_Sim_PyChrono_Docker/deploy/start_container.sh
@@ -8,6 +8,19 @@ xhost +local:docker
 # Create the DockerSharedFolder
 mkdir -p $HOME/DockerSharedFolder
 
+# Add configuration to run VSCode in host with container settings
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p $HOME/DockerSharedFolder/.devcontainer
+cat > $HOME/DockerSharedFolder/.devcontainer/devcontainer.json << EOF
+{
+  "name": "UAV_Sim_PyChrono",
+  "dockerComposeFile": "$SCRIPT_DIR/docker-compose.yml",
+  "service": "uav_sim_pychrono",
+  "workspaceFolder": "/home/user/DockerSharedFolder",
+  "remoteUser": "user"
+}
+EOF
+
 # Launch containers
 docker compose up -d
 
