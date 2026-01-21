20b0a597cdad9707932cecd76397880ecd1cb389
Luca <luca.marchiano@studenti.polito.it>
2026-01-21 10:47:06 -0500

    updates

--- FlightStack_Docker/deploy/entrypoint.sh
+++ FlightStack_Docker/deploy/entrypoint.sh
@@ -54,5 +54,23 @@ EOF
 
 source /home/user/.bashrc
 
+#========================================
+# Add cleanup instructions
+#========================================
+cat >> /home/user/.bashrc << 'EOF'
+cleanup() {
+  echo ""
+  echo "================================================"
+  echo -e "container 'flightstack' closed\n"
+  echo "To perform cleanup run stop_container.sh"
+  echo "================================================"
+
+}
+
+trap cleanup EXIT
+EOF
+
+source /home/user/.bashrc
+
 
 exec "$@"
